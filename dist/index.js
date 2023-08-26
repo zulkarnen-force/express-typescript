1422e02a397a22a1ef271d2a8b647d05e9a27550
zulkarnen <zulkarnen1229@gmail.com>
2023-08-26 09:55:01 +0700

    resolve dependecy

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const UserRouter_1 = __importDefault(require("./routes/UserRouter"));
+const EncounterRouter_1 = __importDefault(require("./routes/EncounterRouter"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
+app.use(body_parser_1.default.json());
 app.use("/users", UserRouter_1.default);
+app.use("/encounters", EncounterRouter_1.default);
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
     mongoose_1.default
